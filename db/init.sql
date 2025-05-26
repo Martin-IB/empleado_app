6d04332db28a670e26792cb0a097a58a9558742b
Martin <mart15chhu@gmail.com>
2025-05-26 17:22:34 -0500

    first commit

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,27 @@
-create database db_poo;
+DROP DATABASE IF EXISTS db_poo;
+CREATE DATABASE db_poo;
+USE db_poo;
 
-use db_poo;
-
+-- Tabla usuarios
 CREATE TABLE usuario (
-  id int(5) NOT NULL primary key auto_increment,
-  nombre varchar(50) NOT NULL,
-  telefono varchar(50) NOT NULL,
-  email varchar(50) NOT NULL
+  id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  telefono VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
 
-insert into usuario(nombre,telefono,email)
-values ('Jaime','999888777','jaime@abc.com');
+INSERT INTO usuario(nombre, telefono, email)
+VALUES ('Jaime', '999888777', 'jaime@abc.com');
+
+-- Tabla empleados
+CREATE TABLE empleados (
+  id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  DNI INT(8) NOT NULL UNIQUE
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
+
+INSERT INTO empleados(nombre, apellido, email, DNI)
+VALUES ('Daniel', 'Montes', 'dan@abc.com', 68382828);
+
